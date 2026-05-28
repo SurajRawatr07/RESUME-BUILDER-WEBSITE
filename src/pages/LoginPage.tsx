3cd70c4c63dbf585b161b689aa99fb0943f2b3cd
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-05-28 10:40:43 +0530

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -6,11 +6,7 @@ import {
   LoorgotPassword,
 }: LoginPageProps) {
   const { login } = useAuth();
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const [showPassword, setShowPassword] = useState(false);
+ssword, setShowPassword] = useState(false);
   const [rememberMe, setRememberMe] = useState(false);
 
   const [error, setError] = useState("");
